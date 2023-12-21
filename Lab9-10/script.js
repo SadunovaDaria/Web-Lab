7b39c53d17bbb4b8effb2a7e1292597fb6d6ae6b
SadunovaDaria <154336451+SadunovaDaria@users.noreply.github.com>
2023-12-21 20:31:49 +0300

    Add files via upload

--- Lab9-10/script.js
+++ Lab9-10/script.js
@@ -1,16 +1,53 @@
-const url = "http://localhost:8000";
-const data = { username: "example" };
+const button = document.getElementById('Sendmessage');
+button.onclick = function validate(){
 
-try {
-  const response = await fetch(url, {
-    method: "POST", // или 'PUT'
-    body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
-  const json = await response.json();
-  console.log("Успех:", JSON.stringify(json));
-} catch (error) {
-  console.error("Ошибка:", error);
+    var x=document.forms['form']['Yourname'].value;
+    var y=document.forms['form']['YourEmail'].value;
+    var z=document.forms['form']['Subject'].value;
+    var f=document.forms['form']['message'].value;
+
+    if (x.length==0){
+        document.getElementById('Yournamex').innerHTML='*данное поле обязательно для заполнения';
+        return false;
+    }
+
+    if (y.length==0){
+        document.getElementById('YourEmailx').innerHTML='*данное поле обязательно для заполнения';
+        return false;
+    }
+
+    at=y.indexOf("@");
+    dot=y.indexOf(".");
+
+    if (at<1 || dot <1){
+        document.getElementById('YourEmailx').innerHTML='*email введен не верно';
+        return false;
+    }
+    if (z.length==0){
+        document.getElementById('Subjectx').innerHTML='*данное поле обязательно для заполнения';
+        return false;
+    }
+    if (f.length==0){
+        document.getElementById('messagex').innerHTML='*данное поле обязательно для заполнения';
+        return false;
+    }
+    sendUser();
+}
+
+
+
+async function sendUser(user) {
+    const response = await fetch("http://localhost:8000", { 
+        mode: 'no-cors',    
+        method: "POST", 
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: document.forms['form']['Yourname'].value,
+                email: document.forms['form']['YourEmail'].value,
+                site: document.forms['form']['Subject'].value,
+                message: document.forms['form']['message'].value
+        })
+    });
+    const responseText = await response.text();
+    console.log(responseText);
 }
--- Lab9-10/server.js
+++ Lab9-10/server.js
@@ -1,6 +1,15 @@
 const http = require("http");
+ 
+http.createServer(async (request, response) => {
+    const buffers = [];
+ 
+    for await (const chunk of request) {
+        buffers.push(chunk);
+    }
+ 
+    const user = JSON.parse(Buffer.concat(buffers).toString());
+    console.log("User name:", user.name+"\n", "User email:", user.email+"\n","User site:",user.site+"\n","Message:", user.message);
 
-const server = http.createServer(function(request, response){
-response.end("Hello METANIT.COM!");
-});
-server.listen(8000); 
+    response.end("Данные успешно получены");
+    
+}).listen(8000, ()=>console.log("Сервер запущен по адресу http://localhost:8000"));
